583afa361d9e302da17eeb9d90e2da308af412c3
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-04 20:02:10 +0300

    commit tree c278b21de958fbcb6c1fe4905681c6071ff6d4c5

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
 
   def index
-    reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    reviews = Review.all.order(rating: :desc) #Returns all reviews order by the rating attributes from the highest.
+    render json: reviews, include: :dog_house #This will include the data for the related dog house and will call the #dog_house instance method that is created with the belongs_to macro
   end
 
 end
--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,3 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews  #Generates an instance method called #.reviews that will help query all the related reviews for the Dog house
 end
